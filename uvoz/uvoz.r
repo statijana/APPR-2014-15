009aee95405ee64efc66607d4a23a1c8224cc2d2
unknown <SlijepcevicT12@R309-05.std.fmf.uni-lj.si>
2015-01-09 10:46:03 +0100

    Popravila uvoz, dodala datoteko grafi.r.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,7 +5,8 @@ uvoziDruzine <- function() {
   return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
                       row.names = 1,
                       col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+                      fileEncoding = "Windows-1250",
+                      row.names=1))
 }
 
 # Zapišimo podatke v razpredelnico druzine.
